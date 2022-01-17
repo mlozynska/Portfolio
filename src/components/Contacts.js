36befb89aafba2887a1a95d2775c78faf3e92c89
Mariana Lozynska <mlozynska79@gmail.com>
2022-01-17 01:31:22 +0000

    concts done

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,13 +1,28 @@
 import React from 'react'
-import { Header, Segment } from 'semantic-ui-react'
+import { Header, Segment, Image, Icon } from 'semantic-ui-react'
 import TopHeader from './TopHeader'
-import Footer from './Footer'
+
+import MLcontacts from '../assets/MLcontacts.png'
 
 const Contacts = () => {
   return (
     <>
       <TopHeader />
-      <Segment>My contacts</Segment>
+      <Header as="h3" id="interests" style={{ color: 'rebeccapurple' }}>
+        CONTACT
+      </Header>
+      <Image id="contactImg" src={MLcontacts} centered rounded />
+      <Segment id="contactInfo" basic style={{ padding: '8px 0 0 0' }}>
+        <a href="https://github.com/mlozynska/">
+          <Icon name="github" size="big" color="black" />
+        </a>
+        <a href="https://www.linkedin.com/in/mariana-lozynska/">
+          <Icon name="linkedin square" size="big" color="black" />
+        </a>
+        <a href="mailto:mlozynska79@gmail.com">
+          <Icon name="mail" size="big" color="black" />
+        </a>
+      </Segment>
     </>
   )
 }
